8d42d36951ebc529ab8abcad719370614eb4d2f5
mael-lukas <mael.lukas@ensea.fr>
2024-11-27 15:48:38 +0100

    updated q2

--- enseash_question2.c
+++ enseash_question2.c
@@ -11,20 +11,22 @@
 int main() {
     int status;
     ssize_t ret;
-    char input[BUFSIZE];
-    ret = read(STDIN_FILENO,input,BUFSIZE);
-    input[ret-1] = '\0';
-
     pid_t pid;
-    pid = fork();
+    char input[BUFSIZE];
+    while(1) {
+         ret = read(STDIN_FILENO,input,BUFSIZE);
+        input[ret-1] = '\0';
 
-    if(pid == 0) {
-        execl(input,input,NULL);
-        exit(EXIT_FAILURE);
+        pid = fork();  
+
+        if(pid == 0) {
+            execl(input,input,NULL);
+            exit(EXIT_FAILURE);
+        }
+        else {
+            wait(&status);
+            write(STDOUT_FILENO,enseash_prompt,strlen(enseash_prompt));
+        }
     }
-    else {
-        wait(&status);
-        write(STDOUT_FILENO,enseash_prompt,9);
-    }
-    return 1;
+    return 0;
 }
