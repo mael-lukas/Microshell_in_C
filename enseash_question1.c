e6861615ae04a7de25881f466e343407529e2fb7
mael-lukas <mael.lukas@ensea.fr>
2024-11-27 18:53:11 +0100

    .

--- enseash_question1.c
+++ enseash_question1.c
@@ -11,5 +11,5 @@
 int main() {
     write(STDOUT_FILENO,welcome_message,strlen(welcome_message)); // STDOUT_FILENO is default output to write in the shell
     write(STDOUT_FILENO,enseash_prompt,strlen(enseash_prompt));
-    return 1;
+    return 0;
 }
